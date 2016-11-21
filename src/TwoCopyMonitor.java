65c5d9295580ed53af30ba376f0237b1520d421a
Brice N <bngnigha@geistglobal.com>
2016-11-21 11:20:29 -0600

    Adding project framework

---
+++ src/TwoCopyMonitor.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author root
+ *
+ */
+public class TwoCopyMonitor {
+
+	/**
+	 * 
+	 */
+	public TwoCopyMonitor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TwoCopyMonitor");
+
+	}
+
+}
