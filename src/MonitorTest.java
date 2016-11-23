50087c9826785d673e165abb88a45c58cb2d63f9
Abed Haque <abedhaque@gmail.com>
2016-11-22 22:50:30 -0600

    Added JUnit test for single-writer multi-reader.

---
+++ src/MonitorTest.java
@@ -0,0 +1,86 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.management.monitor.Monitor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/**
+ * Created by abed on 11/22/16.
+ */
+public class MonitorTest
+{
+    MonitorObj<Integer> monitor;
+
+    @Before public void initialize()
+    {
+        monitor = new SingleCopyMonitor<>(5);
+    }
+
+    @Test
+    public void multipleReadersReadCorrectlyAfterInit()
+    {
+        int numThreads = 100;
+        for( int i = 0; i < numThreads; i++) {
+            Thread readers = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    assertEquals(5, (int)monitor.get());
+                }
+            }
+            );
+
+            readers.start();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void multipleReadersReadCorrectlyAfterWrite()
+    {
+        AtomicInteger v = new AtomicInteger(10000);
+
+        // Single Writer
+        Thread writer = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                do
+                {
+                    monitor.set(v.decrementAndGet());
+                }while (v.get() > 0);
+            }
+        }
+        );
+
+        writer.start();
+
+        // Multiple Readers
+        int numThreads = 100;
+        for( int i = 0; i < numThreads; i++) {
+            Thread readers = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    //System.out.println(Thread.currentThread().getId() + " Monitor:" + monitor.get() );
+                    assertEquals(v.get(), (int)monitor.get());
+                }
+            }
+            );
+
+            readers.start();
+        }
+
+        try {
+            writer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
